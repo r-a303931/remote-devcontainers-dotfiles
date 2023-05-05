0af3c4334c7e94b5cadd645b9c4a1bcbefdd4edc
Andrew Rioux <rioux.d.andrew@gmail.com>
2023-05-05 02:10:10 -0400

    added tmux

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 if which apt;
 then
     sudo apt update
-    sudo apt install -y zsh exa neovim
+    sudo apt install -y zsh exa neovim tmux
 elif which apk;
 then
-    sudo apk add curl zsh exa neovim
+    sudo apk add curl zsh exa neovim tmux
 fi
 
 KEEP_ZSH=yes CHSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
