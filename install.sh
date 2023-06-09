067e10e853965ddc397f70f54c9322fd7e60135e
Andrew Rioux <rioux.d.andrew@gmail.com>
2023-06-09 18:27:11 -0400

    added automatic installation of wakatime
    includes configuration too

--- install.sh
+++ install.sh
@@ -14,3 +14,21 @@ KEEP_ZSH=yes CHSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/
 cp .zshrc $HOME
 cp -r .zshrc.d $HOME
 sudo chsh -s /usr/bin/zsh $USER
+
+WAKATIME_KEY="$(curl https://wakatime-key.svc.arioux.org/arioux-key | tr -d '[:space:]')"
+cat - <<EOF > ~/.wakatime.cfg
+[settings]
+debug         = true
+hidefilenames = false
+ignore        = """
+    COMMIT_EDITMSG$
+    PULLREQ_EDITMSG$
+    MERGE_MSG$
+    TAG_EDITMSG$"""
+api_key       = ${WAKATIME_KEY}
+
+[internal]
+backoff_at      =
+backoff_retries = 0
+EOF
+code --install-extension Wakatime.vscode-wakatime
